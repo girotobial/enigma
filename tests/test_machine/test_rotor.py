d7eeb6feee17cf063e89a1b1989ac7c8bc988d04
girotobial <girotobial@gmail.com>
2021-12-11 22:26:30 +0000

    🧪 add rotor intializer test

---
+++ tests/test_machine/test_rotor.py
@@ -0,0 +1,18 @@
+"""Tests for the rotor of the enigma machine"""
+
+from enigma.machine import rotor
+
+
+def test_rotor_intializes() -> None:
+    """GIVEN a set of inputs
+
+    THEN the rotor intializes
+    AND stores that data
+    """
+    test_rotor = rotor.Rotor("Test", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", 1, 2, 3)
+    assert test_rotor.name == "Test"
+    assert test_rotor.rotor_position == 1
+    assert test_rotor.notch_position == 2
+    assert test_rotor.ring_setting == 3
+    assert test_rotor.forward_wiring == list(range(26))
+    assert test_rotor.backward_wiring == reversed(list(range(26)))
