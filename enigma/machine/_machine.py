ce925cdb1b782ced88dcc6ee6647876a9788cf0c
girotobial <girotobial@gmail.com>
2021-12-12 12:49:12 +0000

    ✨ implement machine reflector

---
+++ enigma/machine/_machine.py
@@ -0,0 +1,59 @@
+from __future__ import annotations
+
+from typing import overload
+
+from .. import core
+from . import reflector, rotor
+
+
+class EnigmaMachine:
+    def __init__(self, rotors: list[rotor.Rotor], reflector_: reflector.Reflector):
+        self.rotors = rotors
+        self.reflector = reflector_
+
+    @classmethod
+    def from_key(cls, key: core.EnigmaKey) -> EnigmaMachine:
+        rotors = [
+            rotor.create_rotor(name, position, ring_setting)
+            for name, position, ring_setting in zip(
+                key.rotors, key.indicators, key.rings
+            )
+        ]
+
+        return cls(rotors, reflector.Reflector.create("B"))
+
+    def rotate(self) -> None:
+        def rotate_rotors(rotors: list[rotor.Rotor]) -> list[rotor.Rotor]:
+            rightmost_rotor = rotors[-1]
+
+            if rightmost_rotor.is_at_notch and len(rotors) != 1:
+                rotors[:-1] = rotate_rotors(rotors[:-1])
+
+            rightmost_rotor.turnover()
+            return rotors
+
+        self.rotors = rotate_rotors(self.rotors)
+
+    @overload
+    def _encrypt(self, character: int) -> int:
+        ...
+
+    @overload
+    def _encrypt(self, character: str) -> str:
+        ...
+
+    def _encrypt(self, character: core.Encypherable) -> core.Encypherable:
+        self.rotate()
+
+        for rotor_ in self.rotors:
+            character = rotor_.forward(character)
+
+        character = self.reflector.forward(character)
+
+        for rotor_ in reversed(self.rotors):
+            character = rotor_.backward(character)
+
+        return character
+
+    def encrypt(self, message: str) -> str:
+        return "".join((self._encrypt(char) for char in message))
---
+++ enigma/machine/reflector.py
@@ -0,0 +1,42 @@
+"""The reflector component of an enigma machine"""
+
+from typing import overload
+
+from .. import core
+from . import wiring
+
+
+class Reflector:
+    def __init__(self, encoding: str):
+        self.wiring = wiring.Wiring(encoding)
+
+    def _forward_int(self, value: int) -> int:
+        return self.wiring[value]
+
+    @overload
+    def forward(self, value: str) -> str:
+        ...
+
+    @overload
+    def forward(self, value: int) -> int:
+        ...
+
+    def forward(self, value: core.Encypherable) -> core.Encypherable:
+        if isinstance(value, int):
+            return self._forward_int(value)
+        if isinstance(value, str) and len(value) == 1:
+            value_int = core.character_to_int(value)
+            enciphered = self._forward_int(value_int)
+            return core.int_to_char(enciphered)
+        raise NotImplementedError
+
+    @classmethod
+    def create(cls, name: str):
+        encodings = {
+            "B": "YRUHQSLDPXNGOKMIEBFZCWVJAT",
+            "C": "FVPJIAOYEDRZXWGCTKUQSBNMHL",
+        }
+
+        if (encoding := encodings.get(name)) is None:
+            encoding = "ZYXWVUTSRQPONMLKJIHGFEDCBA"
+        return cls(encoding)
